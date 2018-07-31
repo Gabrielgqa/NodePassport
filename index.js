f6bcda17876e0bd85a5b3f7a5572274011dcbbc0
GabrielGQA <gabrielgomesqa@dcc.ufba.br>
2018-07-31 15:06:44 -0300

    test wit basic authentication

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
+const passport = require('passport')
 const app = express()
+
+passport.use(require('./src/auth/basic'))
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
+app.use(passport.initialize())
 
+app.get('*', passport.authenticate('basic', { session: false }))
 require('./src/index')(app)
 
 app.listen(9000, () =>{
