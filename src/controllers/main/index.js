8e468a2e40ea56a577c0df1456ce8bedd37615e2
GabrielGQA <gabrielgomesqa@dcc.ufba.br>
2018-08-01 13:31:47 -0300

    signin with passport

--- src/controllers/main/index.js
+++ src/controllers/main/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const isAuth = require('./../../auth/middleware')
 router.get('/', require('./main'))
 
 module.exports = router
