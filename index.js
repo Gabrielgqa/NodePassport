02770c260039ff84f9e7c2e5af74974691472d4f
GabrielGQA <gabrielgomesqa@dcc.ufba.br>
2018-08-01 09:33:55 -0300

    signup with passport

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ const morgan = require('morgan')
 const methodOverride = require('method-override')
 const passport = require('passport')
 const mongoose = require('mongoose')
+const session = require('express-session')
 const app = express()
 
-//passport.use(require('./src/auth/basic'))
-//app.get('*', passport.authenticate('basic', { session: false }))
+require('./src/auth/local')(passport)
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
+app.use(session({ secret: '!#FDSAFDF@!!', resave: false, saveUninitialized: true }))
 app.use(passport.initialize())
+app.use(passport.session())
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'src/views'))
 
-require('./src/index')(app)
+require('./src/index')(app, passport)
 
 mongoose.connect('mongodb://localhost:27017/auth')
 mongoose.Promise = global.Promise
